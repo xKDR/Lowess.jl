1637991c2121ad1530ef51fa362f453dc635d71a
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-03-03 14:21:31 +0530

    Adding tests for Lowess.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using Lowess
 using Test
 
-@testset "Lowess.jl" begin
-    # Write your tests here.
+@testset "Testing Lowess" begin
+    for i in 1:100
+        n = rand(1:100)
+        xs = rand(1:100.0, n)
+        xs = sort(xs)
+        ys = rand(1:100.0, n)
+        zs = lowess(xs, ys, rand(0.1:1.0), rand(3:10), rand(0.1:1.0))
+        @test length(zs) == length(ys)
+    end
 end
+
