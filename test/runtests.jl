a096301afa7266dbeab98b500e76008b8e58505d
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-03-05 12:53:37 +0530

    Making n > 5 in the test code.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "Testing Lowess" begin
     for i in 1:100
-        n = rand(1:100)
+        n = rand(6:100)
         xs = rand(1:100.0, n)
         xs = sort(xs)
         ys = rand(1:100.0, n)
