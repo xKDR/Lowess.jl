5deecc76b2bed8668c467881307af5d273fd96ba
ayushpatnaikgit <ayushpatnaik@gmail.com>
2022-03-11 16:13:27 +0530

    Adding test cases for integer arrays.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Lowess
 using Test
 
-@testset "Testing Lowess" begin
+@testset "Testing with floating point numbers" begin
     for i in 1:100
         n = rand(6:100)
         xs = rand(1:100.0, n)
@@ -12,3 +12,13 @@ using Test
     end
 end
 
+@testset "Testing with integers" begin
+    for i in 1:100
+        n = rand(6:100)
+        xs = rand(1:100, n)
+        xs = sort(xs)
+        ys = rand(1:100, n)
+        zs = lowess(xs, ys, rand(0.1:1.0), rand(3:10), rand(0.1:1.0))
+        @test length(zs) == length(ys)
+    end
+end
